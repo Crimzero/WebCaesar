955701257675961eabb75c68a71e5695ffa332f0
Moore <crimzero@hotmail.com>
2019-01-24 14:09:47 -0600

    Final commit

--- main.py
+++ main.py
@@ -7,39 +7,42 @@ form="""<!DOCTYPE html>
 <html>
     <head>
         <style>
-            
-            form {
+
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
             <form method="post"> 
+                <div>
                 <label for="rot">Rotate by:</label>
                 <input type="text" name="rot" value="0" >
-                <input textarea name="text">
+                </div>
+                <p class=error></p>
+                <textarea type="text" name="text">{0}</textarea>
                 <input type="submit" value="Submit Form">
             </form>
     </body>
 </html>"""
 @app.route("/")
 def index():
-    return form
+    return form.format('')
 @app.route("/", methods=['post'])
 def encrypt():
     text = request.form['text']
     rot = request.form['rot']
     
-    return '<h1>' + rotate_string(text,rot) + '<h1>'
+    return form.format(rotate_string(text,rot))
 
 app.run()
